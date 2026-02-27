a98e59565a5bd4e7818c6d853fc41d5bfd485d96
Ankit Sharma <ankitorg15@gmail.com>
2026-02-27 13:25:59 +0530

    chagnes

---
+++ src/utils/data.js
@@ -0,0 +1,6 @@
+const BASE_URL = "http://147.79.68.143:5011/";
+
+export const USER_API_ENDPOINT = `${BASE_URL}api/user`;
+export const JOB_API_ENDPOINT = `${BASE_URL}api/job`;
+export const APPLICATION_API_ENDPOINT = `${BASE_URL}api/application`;
+export const COMPANY_API_ENDPOINT = `${BASE_URL}api/company`;   
